937890ae46313cee2f61569efabee49066fe1316
dan <themighty1@users.noreply.github.com>
2024-06-06 14:14:43 +0300

    added deadlocking test

--- readme.md
+++ readme.md
@@ -1,94 +1,12 @@
-![MIT licensed][mit-badge]
-![Apache licensed][apache-badge]
+This repo is a fork of https://github.com/tlsnotary/tlsn-js
 
-[mit-badge]: https://img.shields.io/badge/license-MIT-blue.svg
-[apache-badge]: https://img.shields.io/github/license/saltstack/salt
+A test was added to wasm/prover/tests/web.rs which deadlocks when trying to join two futures.
 
-# tlsn-js
-
-NPM Modules for proving and verifying using TLSNotary in the browser.
-
-The prover requires a [notary-server](https://github.com/tlsnotary/notary-server) and a websocket proxy
-
-> [!IMPORTANT]
-> `tlsn-js` is developed for the usage of TLSNotary **in the Browser**. This module does not work in `nodejs`.
-
-## License
-This repository is licensed under either of
-
-- [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0)
-- [MIT license](http://opensource.org/licenses/MIT)
-
-at your option.
-
-## Example
-```ts
-import { prove, verify } from '../src';
-
-// To create a proof
-const proof = await prove('https://swapi.dev/api/people/1', {
-    method: 'GET',
-    headers: {
-      Connection: 'close',
-      Accept: 'application/json',
-      'Accept-Encoding': 'identity',
-    },
-    body: '',
-    maxTranscriptSize: 20000,
-    notaryUrl: 'https://127.0.0.1:7047',
-    websocketProxyUrl: 'ws://127.0.0.1:55688',
-});
-
-// To verify a proof
-const result = await verify(proof);
-console.log(result);
-```
-
-## Running a local websocket proxy for `https://swapi.dev`
-
-1. Install [websocat](https://github.com/vi/websocat):
-
-    | tool   | command                       |
-    |--------|-------------------------------|
-    | cargo  | `cargo install websocat`      |
-    | brew   | `brew install websocat`       |
-    | source | https://github.com/vi/websocat|
-
-2. Run a websocket proxy for `https://swapi.dev`:
-```sh
-websocat --binary -v ws-l:0.0.0.0:55688 tcp:swapi.dev:443
-```
-
-## Install as NPM Package
+To reproduce, run
 
 ```
-npm install tlsn-js
+cd wasm/prover
+wasm-pack test --firefox --release --headles
 ```
 
-## Development
-
-> [!IMPORTANT]
-> **Note on Rust-to-WASM Compilation**: This project requires compiling Rust into WASM, which needs [`clang`](https://clang.llvm.org/) version 16.0.0 or newer. MacOS users, be aware that Xcode's default `clang` might be older. If you encounter the error `No available targets are compatible with triple "wasm32-unknown-unknown"`, it's likely due to an outdated `clang`. Updating `clang` to a newer version should resolve this issue.
-
-```
-# make sure you have rust installed
-# https://www.rust-lang.org/tools/install
-npm install
-
-# this serve a page that will execute the example code at http://localhost:3001 
-npm run dev
-```
-
-## Build for NPM
-
-```
-npm install
-npm run build
-```
-
-## Adding a new test
-1. Create a new `new-test.spec.ts` file in the `test/` directory
-2. Add your spec file to the entry object fin `webpack.web.dev.config.js`
-3. Add a new `div` block to `test/test.ejs` like this: `<div>Testing "new-test":<div id="new-test"></div></div>`. The div id must be the same as the filename.
-
-
+After ~20 secs there will be an error caused by a timeout.
--- wasm/prover/tests/web.rs
+++ wasm/prover/tests/web.rs
@@ -59,3 +59,90 @@ async fn verify() {
     assert_eq!(r.sent, m["sent"].as_str().unwrap());
     assert_eq!(r.recv, m["recv"].as_str().unwrap());
 }
+
+use futures::{channel::oneshot, future::TryFutureExt};
+use mpz_garble::{protocol::deap::mock::create_mock_deap_vm, Memory, Vm};
+use tlsn_block_cipher::{Aes128, BlockCipher, BlockCipherConfig, MpcBlockCipher};
+use wasm_bindgen_futures::spawn_local;
+
+// This test was copied from tlsn/components/cipher/block-cipher/src/lib.rs
+#[wasm_bindgen_test]
+async fn test_block_cipher_share() {
+    let leader_config = BlockCipherConfig::builder().id("test").build().unwrap();
+    let follower_config = BlockCipherConfig::builder().id("test").build().unwrap();
+
+    let key = [0u8; 16];
+
+    let (mut leader_vm, mut follower_vm) = create_mock_deap_vm("test").await;
+    let leader_thread = leader_vm.new_thread("test").await.unwrap();
+    let follower_thread = follower_vm.new_thread("test").await.unwrap();
+
+    // Key is public just for this test, typically it is private.
+    let leader_key = leader_thread.new_public_input::<[u8; 16]>("key").unwrap();
+    let follower_key = follower_thread.new_public_input::<[u8; 16]>("key").unwrap();
+
+    leader_thread.assign(&leader_key, key).unwrap();
+    follower_thread.assign(&follower_key, key).unwrap();
+
+    let mut leader = MpcBlockCipher::<Aes128, _>::new(leader_config, leader_thread);
+    leader.set_key(leader_key);
+
+    let mut follower = MpcBlockCipher::<Aes128, _>::new(follower_config, follower_thread);
+    follower.set_key(follower_key);
+
+    let plaintext = [0u8; 16];
+
+    // // Commenting out the original code which does not compile to wasm
+    //
+    // let (leader_share, follower_share) = tokio::try_join!(
+    //     leader.encrypt_share(plaintext.to_vec()),
+    //     follower.encrypt_share(plaintext.to_vec())
+    // )
+    // .unwrap();
+
+    // //Approach 1: using futures::try_join! causes a deadlock
+    // log!("will join");
+
+    // let (leader_share, follower_share) = futures::try_join!(
+    //     leader.encrypt_share(plaintext.to_vec()),
+    //     follower.encrypt_share(plaintext.to_vec())
+    // )
+    // .unwrap();
+
+    // log!("futures resolved");
+
+    // // Approach 2: using spawn_local() causes a deadlock
+    let (tx1, rx1) = oneshot::channel();
+    let (tx2, rx2) = oneshot::channel();
+    let fut1 = async move {
+        let res = leader.encrypt_share(plaintext.to_vec()).await.unwrap();
+        log!("here 1");
+        _ = tx1.send(res).unwrap();
+    };
+    spawn_local(fut1);
+
+    let fut2 = async move {
+        let res = follower.encrypt_share(plaintext.to_vec()).await.unwrap();
+        log!("here 2");
+        _ = tx2.send(res).unwrap();
+    };
+    spawn_local(fut2);
+
+    log!("waiting for fut1 to resolve");
+
+    let val1 = rx1.await.unwrap();
+
+    log!("resolved 1");
+
+    let val2 = rx2.await.unwrap();
+
+    log!("resolved 2");
+
+    let (leader_share, follower_share) = (val1, val2);
+
+    // let expected = aes128(key, plaintext);
+
+    // let result: [u8; 16] = std::array::from_fn(|i| leader_share[i] ^ follower_share[i]);
+
+    // assert_eq!(result, expected);
+}
